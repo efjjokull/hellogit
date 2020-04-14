96f3c429c915572258a8ea17fc50fc6244c62276
efjjokull <63567709+efjjokull@users.noreply.github.com>
2020-04-14 20:30:11 +0900

    Update script.js

--- iris/script.js
+++ iris/script.js
@@ -4,7 +4,7 @@ async function fnc(){
     var b = parseFloat(document.getElementById("b").value);
     var c = parseFloat(document.getElementById("c").value);
     var d = parseFloat(document.getElementById("d").value);
-    const model = await tf.loadLayersModel("iris/model/model.json");
+    const model = await tf.loadLayersModel("https://efjjokull.github.io/hellogit/iris/model/model.json");
     const input = tf.tensor([a,b,c,d]);
     let prediction = await model.predict(tf.reshape(input,[1,4])).data();
 
@@ -14,7 +14,7 @@ async function fnc(){
 }
 
 async function load_Model(){
-    const model = await tf.loadLayersModel("iris/model/model.json");
+    const model = await tf.loadLayersModel("https://efjjokull.github.io/hellogit/iris/model/model.json");
 <<<<<<< HEAD
 }
 =======
