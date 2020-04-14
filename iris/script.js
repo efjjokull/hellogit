cca88d49c2fe528de0891fe07e9cca0f64cc2e4b
efjjokull <gy39tbcx@gmail.com>
2020-04-14 20:14:18 +0900

    2nd

--- iris/script.js
+++ iris/script.js
@@ -1,9 +1,10 @@
+
 async function fnc(){
     var a = parseFloat(document.getElementById("a").value);
     var b = parseFloat(document.getElementById("b").value);
     var c = parseFloat(document.getElementById("c").value);
     var d = parseFloat(document.getElementById("d").value);
-    const model = await tf.loadLayersModel("http://localhost:8000/Desktop/iris/model/model.json");
+    const model = await tf.loadLayersModel("iris/model/model.json");
     const input = tf.tensor([a,b,c,d]);
     let prediction = await model.predict(tf.reshape(input,[1,4])).data();
 
@@ -13,5 +14,5 @@ async function fnc(){
 }
 
 async function load_Model(){
-    const model = await tf.loadLayersModel("http://localhost:8000/Desktop/iris/model/model.json")
+    const model = await tf.loadLayersModel("iris/model/model.json");
 }
